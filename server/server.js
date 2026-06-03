98df24318741424a9eddbaa24d0cccb2188dc97e
Le Hoang <ndlhoangndl@gmail.com>
2026-06-03 14:29:14 +0700

    feat: add healthz and root routes for hosting health checks

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Health check and root route
+app.get('/healthz', (req, res) => res.status(200).send('OK'));
+app.get('/', (req, res) => res.status(200).send('Server is running'));
+
 // Connect to MongoDB Atlas and Seed default admin
 connectDB();
 
